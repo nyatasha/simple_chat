e67ac12dc9ca0f78f505fc7103cefb6053f05180
nyatashak <softkitty1>
2017-11-12 20:29:25 +0300

    test connection added

--- src/main/scala/rmi/RMIChatServer.scala
+++ src/main/scala/rmi/RMIChatServer.scala
@@ -13,12 +13,15 @@ import scala.collection.mutable
   def getDeadClients: Seq[RemoteClient]
   def publicMessage(client: RemoteClient, text: String): Unit
 }
-object RMIChatServer extends UnicastRemoteObject with App with RemoteServer {
-  LocateRegistry.createRegistry(1099)
-  Naming.rebind("ChatServer", this)
+object RMIChatServer extends UnicastRemoteObject with RemoteServer {
 
   private val clients = mutable.Buffer[RemoteClient]()
   private val deadClients = mutable.Buffer[RemoteClient]()
+
+  def main()={
+    LocateRegistry.createRegistry(1099)
+    Naming.rebind("ChatServer", this)
+  }
 
   def connect(client: RemoteClient): Unit = {
     if (deadClients contains client)
--- src/test/scala/rmi/ChatSuite.scala
+++ src/test/scala/rmi/ChatSuite.scala
@@ -5,39 +5,24 @@ import java.rmi.Naming
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
-
+import scalafx.Includes._
 import scalafx.scene.control.TextInputDialog
 
 @RunWith(classOf[JUnitRunner])
 class ChatSuite extends FunSuite {
   test("connection to server") {
+    RMIChatServer.main()
     var ok = true
-    val m = "localhost" match {
-      case machine =>
-        Naming.lookup(s"rmi://$machine/ChatServer") match {
-          case server: RemoteServer =>
-              ("test", server)
-              ok = true
-          case _ =>
-            println("There were problems.")
-            ok = false
-            sys.exit(0)
-        }
+    val machine = "localhost"
+    Naming.lookup(s"rmi://$machine/ChatServer") match {
+      case server: RemoteServer =>
+        ("test", server)
+        ok = true
       case _ =>
+        println("There were problems.")
         ok = false
         sys.exit(0)
     }
-/*      val machine = "localhost"
-      var ok = true
-      Naming.lookup(s"rmi://$machine/ChatServer") match {
-        case server: RemoteServer =>
-          ("test", server)
-          ok = true
-        case _ =>
-          println("There were problems.")
-          ok = false
-      }*/
-      assert(ok === true)
+    assert(ok === true)
   }
-
 }
